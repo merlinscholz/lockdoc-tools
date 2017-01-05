18564a557fff672419f5ee5db473a5a4bcab6e20
Alexander Lochmann <alexander.lochmann@tu-dortmund.de>
2017-01-05 11:26:51 +0100

    import.sh: Allow to specify a certain table to import

--- import.sh
+++ import.sh
@@ -7,6 +7,7 @@ fi
 
 DB=${1}
 DELIMITER=';'
+TABLE_CMDLINE=${2}
 
 TABLES=("data_types" "allocations" "accesses" "locks" "locks_held" "structs_layout" "txns" "blacklist")
 
@@ -14,19 +15,28 @@ function mysqlimport_warnings() {
 mysql -vvv --show-warnings --execute="LOAD DATA LOCAL INFILE '$2' INTO TABLE ${2%%.csv} FIELDS TERMINATED BY '$DELIMITER' IGNORE 1 LINES" $1
 }
 
+function import_table() {
+	if [ ! -f ${1}.csv ];
+	then
+		echo "${1}.csv does not exist."
+	else
+		echo "Truncating ${1} ..."
+		echo "DELETE FROM ${1}" | ${MYSQL}
+		echo "Importing ${1}.csv ..."
+		${MYSQLIMPORT} ${1}.csv
+	fi	
+}
+
 MYSQL="mysql -vvv ${DB}"
 #MYSQLIMPORT="mysqlimport --local --fields-terminated-by=${DELIMITER} --ignore-lines=1 -v ${DB}"
 MYSQLIMPORT="mysqlimport_warnings ${DB}"
 
-for table in "${TABLES[@]}"
-do
-	if [ ! -f ${table}.csv ];
-	then
-		echo "${table}.csv does not exist."
-	else
-		echo "Truncating ${table} ..."
-		echo "DELETE FROM ${table}" | ${MYSQL}
-		echo "Importing ${table}.csv ..."
-		${MYSQLIMPORT} ${table}.csv
-	fi
-done
+if [ ! -z ${TABLE_CMDLINE} ];
+then
+	import_table ${TABLE_CMDLINE}
+else
+	for table in "${TABLES[@]}"
+	do
+			import_table ${table}
+	done
+fi
