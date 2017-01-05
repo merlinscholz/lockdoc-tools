445c91c0ff6a1cf8a7cdba53944d47b3f8f1e3b4
Alexander Lochmann <alexander.lochmann@tu-dortmund.de>
2017-01-05 18:06:13 +0100

    Queries++: Find all accesses by functionename without any lock beeing held

---
+++ queries/accesses_without_lock_by_fn.sql
@@ -0,0 +1,41 @@
+SELECT
+	ac_type,
+	ac_fn,
+	sl_member,
+	COUNT(*) AS num
+FROM
+(
+	SELECT
+		ac.id AS ac_id,
+		ac.txn_id AS ac_txn_id,
+		ac.type AS ac_type,
+		ac.fn AS ac_fn,
+		sl.member AS sl_member
+	FROM accesses AS ac
+	INNER JOIN allocations AS a ON a.id=ac.alloc_id
+	INNER JOIN data_types AS dt ON dt.id=a.type
+	LEFT JOIN structs_layout AS sl ON sl.type_id=a.type AND (ac.address - a.ptr) >= sl.offset AND (ac.address - a.ptr) < sl.offset+sl.size
+	WHERE 
+		a.type = 2 AND
+		ac.type  IN ('r','w') AND
+		ac.fn NOT IN
+		(
+			SELECT bl.fn
+			FROM blacklist AS bl
+			WHERE
+			bl.datatype_id = a.type AND
+			(
+				bl.datatype_member IS NULL OR
+				bl.datatype_member = sl.member
+			)
+		)
+	GROUP BY ac.id
+) s
+LEFT JOIN locks_held AS lh ON lh.txn_id=ac_txn_id
+LEFT JOIN locks AS l ON l.id=lh.lock_id
+LEFT JOIN allocations AS a2 ON a2.id=l.embedded_in
+LEFT JOIN structs_layout AS sl2 ON sl2.type_id=a2.type AND (l.ptr - a2.ptr) >= sl2.offset AND (l.ptr - a2.ptr) < sl2.offset+sl2.size
+ WHERE
+	lh.start IS NULL
+GROUP BY ac_type, sl_member, ac_fn
+ORDER BY ac_type, sl_member, ac_fn, num DESC;
