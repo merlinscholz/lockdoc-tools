1d112cbfe15991f8e300700f9ddc18b0c28797db
Alexander Lochmann <alexander.lochmann@tu-dortmund.de>
2017-04-01 20:25:08 +0200

    Queries: Use member blacklist and flatten structs_layout in accesses-without-locks query

--- queries/accesses_without_lock_by_fn.sql
+++ queries/accesses_without_lock_by_fn.sql
@@ -10,28 +10,37 @@ FROM
 		ac.txn_id AS ac_txn_id,
 		ac.type AS ac_type,
 		ac.fn AS ac_fn,
-		mn.name AS sl_member
+		mn.name AS sl_member,
+		sl.member_id AS sl_member_id
 	FROM accesses AS ac
 	INNER JOIN allocations AS a ON a.id=ac.alloc_id
 	INNER JOIN data_types AS dt ON dt.id=a.type
-	LEFT JOIN structs_layout AS sl ON sl.type_id=a.type AND (ac.address - a.ptr) >= sl.offset AND (ac.address - a.ptr) < sl.offset+sl.size
-	LEFT JOIN member_names AS mn ON mn.id = sl.member_id
+	LEFT JOIN structs_layout_flat sl
+	  ON a.type = sl.type_id
+	 AND ac.address - a.ptr = sl.helper_offset
+	LEFT JOIN member_names AS mn
+	  ON mn.id = sl.member_id
 	LEFT JOIN function_blacklist fn_bl
 	  ON fn_bl.datatype_id = a.type
 	 AND fn_bl.fn = ac.fn
 	 AND (fn_bl.datatype_member_id IS NULL OR fn_bl.datatype_member_id = sl.member_id)
-	WHERE 
-		a.type = (SELECT id FROM data_types WHERE name = 'inode') AND
-		ac.type  IN ('r','w') AND
-		fn_bl.fn IS NULL
+	LEFT JOIN member_blacklist m_bl
+	  ON m_bl.datatype_id = a.type
+	 AND m_bl.datatype_member_id = sl.member_id
+	WHERE
+		a.type IN (SELECT id FROM data_types WHERE name IN ('journal_t','transaction_t')) AND
+		fn_bl.fn IS NULL AND
+		m_bl.datatype_member_id IS NULL
 	GROUP BY ac.id
 ) s
 LEFT JOIN locks_held AS lh ON lh.txn_id=ac_txn_id
 LEFT JOIN locks AS l ON l.id=lh.lock_id
 LEFT JOIN allocations AS a2 ON a2.id=l.embedded_in
-LEFT JOIN structs_layout AS sl2 ON sl2.type_id=a2.type AND (l.ptr - a2.ptr) >= sl2.offset AND (l.ptr - a2.ptr) < sl2.offset+sl2.size
-	LEFT JOIN member_names AS mn2 ON mn2.id = sl2.member_id
+LEFT JOIN structs_layout_flat sl2
+	  ON a2.type = sl2.type_id
+	 AND l.ptr - a2.ptr = sl2.helper_offset
+LEFT JOIN member_names AS mn2 ON mn2.id = sl2.member_id
  WHERE
 	lh.start IS NULL
-GROUP BY ac_type, sl_member, ac_fn
-ORDER BY ac_type, sl_member, ac_fn, num DESC;
+GROUP BY ac_type, sl_member_id, ac_fn
+ORDER BY ac_type, sl_member_id, ac_fn, num DESC;
