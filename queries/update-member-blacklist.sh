c4fc687a14ce23cee7f5d56e154077831723bf9e
Alexander Lochmann <alexander.lochmann@tu-dortmund.de>
2017-04-12 15:04:32 +0200

    update-member-blacklist.sh: Allow updating of multiple databases at a time

--- queries/update-member-blacklist.sh
+++ queries/update-member-blacklist.sh
@@ -6,22 +6,31 @@
 
 if [ ${#} -lt 3 ];
 then
-	echo "usage: ${0} <database> <data type> <member name 1> [<member name 2> ...]" >&2
+	echo "usage: ${0} <database> <data type> <member name 1> [<member name 2> ...]
+		  database might be a list of databases separated by a comma." >&2
 	exit 1
 fi
 
-DB=${1}; shift
-DATA_TYPE=${1}; shift
-MEMBER_NAME=${1}; shift
+DATABASES=${1}
+DATA_TYPE=${2}
 MYSQL="mysql ${DB}"
+COUNT=1
+IFS=','
 
-while [ ! -z ${MEMBER_NAME} ];
+for db in ${DATABASES};
 do
-	echo "Blacklisting ${DATA_TYPE}.${MEMBER_NAME}..."
-	${MYSQL} <<EOT
+	echo "Inserting into database ${db}..."
+	COUNT=3
+
+	while [ ! -z ${!COUNT} ];
+	do
+		MEMBER_NAME=${!COUNT}
+		echo "   Blacklisting: ${DATA_TYPE}.${MEMBER_NAME}"
+		${MYSQL} <<EOT
 select id into @dtid from data_types where name = '${DATA_TYPE}';
 select id into @mnid from member_names where name = '${MEMBER_NAME}';
 insert into member_blacklist values (@dtid,@mnid);
 EOT
-	MEMBER_NAME=${1}; shift
+		let COUNT=COUNT+1
+	done
 done
