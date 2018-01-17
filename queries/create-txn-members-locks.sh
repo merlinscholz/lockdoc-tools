45b272e1e3c496f1cc5314ea5f76d1cb12c85a81
Alexander Lochmann <alexander.lochmann@tu-dortmund.de>
2018-01-17 08:34:35 +0100

    create-txn-members-locks.sh: Updated stack query to *really* count stacktraces

--- queries/create-txn-members-locks.sh
+++ queries/create-txn-members-locks.sh
@@ -15,6 +15,8 @@ fi
 
 MODE=${MODE:-nostack}
 
+if [ ${MODE} == "nostack" ];
+then
 cat <<EOT
 -- Count member-access combinations within distinct TXNs, allocations, and
 -- locks-held sets (the latter also taking lock order into account).
@@ -53,14 +55,6 @@ FROM
 	(
 		-- GROUP_CONCAT all member accesses within a TXN and a specific allocation
 		SELECT dt.id AS type_id, dt.name AS type_name, fac.alloc_id, fac.txn_id,
-EOT
-if [ ${MODE} == "stack" ];
-then
-cat <<EOT
-			fac.instrptr, fac.stacktrace_id,
-EOT
-fi
-cat <<EOT
 			GROUP_CONCAT(CONCAT(fac.type, ':', fac.member) ORDER BY fac.offset) AS members_accessed
 		FROM
 
@@ -71,14 +65,6 @@ cat <<EOT
 			-- NOTE: The above property does *NOT* apply if the the results are grouped by instrptr and stacktrace_id.
 			-- NOTE: This does not fold accesses to two different allocations.
 			SELECT ac.alloc_id, ac.txn_id, MAX(ac.type) AS type, a.type AS type_id, mn.name AS member, sl.offset, sl.size
-EOT
-if [ ${MODE} == "stack" ];
-then
-cat <<EOT
-			, ac.instrptr, ac.stacktrace_id
-EOT
-fi
-cat <<EOT
 			FROM accesses ac
 			JOIN allocations a
 			  ON ac.alloc_id = a.id
@@ -106,27 +92,11 @@ cat <<EOT
 			AND m_bl.datatype_member_id IS NULL
 			AND ac.txn_id IS NOT NULL
 			GROUP BY ac.alloc_id, ac.txn_id, a.type, sl.offset
-EOT
-if [ ${MODE} == "stack" ];
-then
-cat <<EOT
-			, ac.instrptr, ac.stacktrace_id
-EOT
-fi
-cat <<EOT
 		) AS fac -- = Folded ACcesses
 
 		JOIN data_types dt
 		  ON dt.id = fac.type_id
 		GROUP BY fac.alloc_id, fac.txn_id, dt.id
-EOT
-if [ ${MODE} == "stack" ];
-then
-cat <<EOT
-			, fac.instrptr, fac.stacktrace_id
-EOT
-fi
-cat <<EOT
 	) AS concatgroups
 
 	JOIN locks_held lh
@@ -150,14 +120,6 @@ cat <<EOT
 	  ON mn_lock_member.id = lock_member.member_id
 
 	GROUP BY concatgroups.alloc_id, concatgroups.txn_id, concatgroups.type_id
-EOT
-if [ ${MODE} == "stack" ];
-then
-cat <<EOT
-			, concatgroups.instrptr, concatgroups.stacktrace_id
-EOT
-fi
-cat <<EOT
 
 	UNION ALL
 
@@ -199,3 +161,118 @@ GROUP BY type_id, members_accessed, locks_held
 ORDER BY type_id, occurrences, members_accessed, locks_held
 ;
 EOT
+elif [ ${MODE} == "stack" ];
+then
+cat <<EOT
+SELECT 
+	type_name,
+	member, locks_held,
+	COUNT(*) AS occurrences
+FROM
+(
+	SELECT 
+		type_name, type_id,
+		member, locks_held
+	FROM
+	(
+		SELECT fac.type_id, dt.name AS type_name, fac.member, fac.instrptr, fac.stacktrace_id,
+			GROUP_CONCAT(
+				CASE
+				WHEN l.embedded_in IS NULL THEN CONCAT(l.id, '(', l.type, ')') -- global (or embedded in unknown allocation)
+				WHEN l.embedded_in IS NOT NULL AND l.embedded_in = fac.alloc_id
+					THEN CONCAT('EMBSAME(', IF(l.ptr - lock_a.ptr = lock_member.offset, mn_lock_member.name, CONCAT(mn_lock_member.name, '?')), ')') -- embedded in same
+				ELSE CONCAT('EMB:', l.id, '(',  IF(l.ptr - lock_a.ptr = lock_member.offset, mn_lock_member.name, CONCAT(mn_lock_member.name, '?')), ')') -- embedded in other
+				END
+				ORDER BY lh.start
+			) AS locks_held
+		FROM
+		(
+			SELECT ac.id, ac.alloc_id, ac.txn_id, ac.type AS ac_type, a.type AS type_id, CONCAT(ac.type, ':', mn.name) AS member, sl.offset, ac.instrptr, ac.stacktrace_id
+			FROM accesses ac
+			JOIN allocations a
+			  ON ac.alloc_id = a.id
+			LEFT JOIN structs_layout_flat sl
+			  ON a.type = sl.type_id
+			 AND ac.address - a.ptr = sl.helper_offset
+			LEFT JOIN member_names mn
+			  ON mn.id = sl.member_id
+			LEFT JOIN function_blacklist fn_bl
+			  ON fn_bl.datatype_id = a.type
+			 AND fn_bl.fn = ac.fn
+			 AND (fn_bl.datatype_member_id IS NULL OR fn_bl.datatype_member_id = sl.member_id)
+			LEFT JOIN member_blacklist m_bl
+			  ON m_bl.datatype_id = a.type
+			 AND m_bl.datatype_member_id = sl.member_id
+			WHERE 1
+			${DATATYPE_FILTER}
+			${MEMBER_FILTER}
+			-- === FOR NOW: skip task_struct ===
+			AND a.type != (SELECT id FROM data_types WHERE name = 'task_struct')
+			-- ====================================
+			AND fn_bl.fn IS NULL
+			AND m_bl.datatype_member_id IS NULL
+			AND ac.txn_id IS NOT NULL
+			GROUP BY ac.id -- Remove duplicate entries. Some accesses might be mapped to more than one member, e.g., an union.
+		) AS fac
+		JOIN data_types dt
+			ON dt.id = fac.type_id
+		JOIN locks_held lh
+		  ON lh.txn_id = fac.txn_id
+		JOIN locks l
+		  ON l.id = lh.lock_id
+		-- find out more about each held lock (allocation -> structs_layout
+		-- member or contained-in member in case of a complex member)
+		LEFT JOIN allocations lock_a
+		  ON l.embedded_in = lock_a.id
+		--	LEFT JOIN data_types lock_a_dt
+		--	  ON lock_a.type = lock_a_dt.id
+		LEFT JOIN structs_layout_flat lock_member
+		  ON lock_a.type = lock_member.type_id
+		 AND l.ptr - lock_a.ptr = lock_member.helper_offset
+		-- lock_a.id IS NULL                         => not embedded
+		-- l.ptr - lock_a.ptr = lock_member.offset   => the lock is exactly this member (or at the beginning of a complex sub-struct)
+		-- else                                      => the lock is contained in this member, exact name unknown
+		LEFT JOIN member_names mn_lock_member
+		  ON mn_lock_member.id = lock_member.member_id
+		GROUP BY fac.id
+		
+		UNION ALL
+		
+		SELECT a.type AS type_id, dt.name AS type_name, CONCAT(ac.type, ':', mn.name) AS member, ac.instrptr, ac.stacktrace_id, '' AS locks_held
+		FROM accesses ac
+		JOIN allocations a
+		  ON ac.alloc_id = a.id
+		LEFT JOIN structs_layout_flat sl
+		  ON a.type = sl.type_id
+		 AND ac.address - a.ptr = sl.helper_offset
+		LEFT JOIN member_names mn
+		  ON mn.id = sl.member_id
+		LEFT JOIN function_blacklist fn_bl
+		  ON fn_bl.datatype_id = a.type
+		 AND fn_bl.fn = ac.fn
+		 AND (fn_bl.datatype_member_id IS NULL OR fn_bl.datatype_member_id = sl.member_id)
+		LEFT JOIN member_blacklist m_bl
+		  ON m_bl.datatype_id = a.type
+		 AND m_bl.datatype_member_id = sl.member_id
+		JOIN data_types dt
+			ON dt.id = a.type
+		WHERE 1
+		${DATATYPE_FILTER}
+		${MEMBER_FILTER}
+		-- === FOR NOW: skip task_struct ===
+		AND a.type != (SELECT id FROM data_types WHERE name = 'task_struct')
+		-- ====================================
+		AND fn_bl.fn IS NULL
+		AND m_bl.datatype_member_id IS NULL
+		AND ac.txn_id IS NULL
+		GROUP BY ac.id
+	) AS concatlocks	
+	GROUP BY concatlocks.type_id, concatlocks.member, concatlocks.locks_held, concatlocks.instrptr, concatlocks.stacktrace_id
+) AS fstacks
+GROUP BY fstacks.type_id, fstacks.member, fstacks.locks_held
+ORDER BY fstacks.type_id, fstacks.member, fstacks.locks_held, occurrences
+;
+EOT
+else
+	echo "Unknown mode!"
+fi
