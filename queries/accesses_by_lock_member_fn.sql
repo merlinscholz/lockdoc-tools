11bf83539767f00fbc7a004ecdadb1f9c076e8b1
Alexander Lochmann <alexander.lochmann@tu-dortmund.de>
2018-01-29 13:58:40 +0100

    accesses_by_lock_member_fn.sql: Fetch locks held, and group it by them.

--- queries/accesses_by_lock_member_fn.sql
+++ queries/accesses_by_lock_member_fn.sql
@@ -1,57 +1,82 @@
-SELECT
-	ac_type,
-	sl_member,
-	IF(l.type IS NULL, 'null',
-	  IF(mn2.name IS NULL,CONCAT('global_',l.type,'_',l.id),CONCAT(mn2.name,'_',IF(l.embedded_in = alloc_id,'1','0')))) AS lock_member,
-	ac_fn,
-	lower(hex(ac_instrptr)) AS ac_instrptr,
-	CASE 
-		WHEN lh.lastPreemptCount & 0x0ff00 THEN 'softirq'
-		WHEN lh.lastPreemptCount & 0xf0000 THEN 'hardirq'
-		WHEN (lh.lastPreemptCount & 0xfff00) = 0 THEN 'noirq'
-		WHEN lh.lastPreemptCount IS NULL THEN 'nolock'
-		ELSE 'unknown'
-	END AS context, lh.lastPreemptCount AS preemptCount,
-	COUNT(*) AS num
+SELECT ac_type, sl_member, locks_held, ac_fn, ac_instrptr, contexts, preemptCounts, COUNT(*) AS num
 FROM
 (
-	-- Get all accesses. Add information about the accessed member, data type, and the function the memory has been accessed from.
-	-- Filter out every function that is on our function blacklist.
 	SELECT
-		ac.id AS ac_id,
-		ac.txn_id AS ac_txn_id,
-		ac.alloc_id AS alloc_id,
-		ac.type AS ac_type,
-		ac.fn AS ac_fn,
-		ac.address AS ac_address,
-		a.ptr AS a_ptr,
-		ac.instrptr AS ac_instrptr,
-		mn.name AS sl_member,
-		dt.name AS dt_name
-	FROM accesses AS ac
-	INNER JOIN allocations AS a ON a.id=ac.alloc_id
-	INNER JOIN data_types AS dt ON dt.id=a.type
-	LEFT JOIN structs_layout_flat sl
-	  ON a.type = sl.type_id
-	 AND ac.address - a.ptr = sl.helper_offset
-	LEFT JOIN member_names AS mn ON mn.id = sl.member_id
-	LEFT JOIN function_blacklist fn_bl
-	  ON fn_bl.datatype_id = a.type
-	 AND fn_bl.fn = ac.fn
-	 AND (fn_bl.datatype_member_id IS NULL OR fn_bl.datatype_member_id = sl.member_id)
-	WHERE 
-		-- Name the data type of interest here
-		a.type = (SELECT id FROM data_types WHERE name = 'transaction_t') AND
-		ac.type  IN ('w') AND -- Filter by access type
---		sl.member_id IN (SELECT id FROM member_names WHERE name in ('t_tid')) AND -- Only show results for a certain member
-		fn_bl.fn IS NULL
-	GROUP BY ac.id -- Remove duplicate entries. Some accesses might be mapped to more than one member, e.g., an union.
-) s
-LEFT JOIN locks_held AS lh ON lh.txn_id=ac_txn_id
-LEFT JOIN locks AS l ON l.id=lh.lock_id
-LEFT JOIN allocations AS a2 ON a2.id=l.embedded_in
-LEFT JOIN structs_layout AS sl2 ON sl2.type_id=a2.type AND (l.ptr - a2.ptr) >= sl2.offset AND (l.ptr - a2.ptr) < sl2.offset+sl2.size
-LEFT JOIN member_names AS mn2 ON mn2.id = sl2.member_id
+		ac_type,
+		sl_member,
+		GROUP_CONCAT(
+		CASE
+			WHEN l.embedded_in IS NULL THEN CONCAT(l.id, '(', l.type, ')') -- global (or embedded in unknown allocation)
+			WHEN l.embedded_in IS NOT NULL AND l.embedded_in = alloc_id
+				THEN CONCAT('EMBSAME(', CONCAT(lock_a_dt.name, '.', IF(l.ptr - lock_a.ptr = lock_member.offset, mn_lock_member.name, CONCAT(mn_lock_member.name, '?'))), ')') -- embedded in same
+			ELSE CONCAT('EMBOTHER', '(',  CONCAT(lock_a_dt.name, '.', IF(l.ptr - lock_a.ptr = lock_member.offset, mn_lock_member.name, CONCAT(mn_lock_member.name, '?'))), ')') -- embedded in other
+--			ELSE CONCAT('EMB:', l.id, '(',  CONCAT(lock_a_dt.name, '.', IF(l.ptr - lock_a.ptr = lock_member.offset, mn_lock_member.name, CONCAT(mn_lock_member.name, '?'))), ')') -- embedded in other
+			END
+			ORDER BY lh.start
+			SEPARATOR ' -> '
+		) AS locks_held,
+		ac_fn,
+		lower(hex(ac_instrptr)) AS ac_instrptr,
+		GROUP_CONCAT(
+			CASE 
+			WHEN lh.lastPreemptCount & 0x0ff00 THEN 'softirq'
+			WHEN lh.lastPreemptCount & 0xf0000 THEN 'hardirq'
+			WHEN (lh.lastPreemptCount & 0xfff00) = 0 THEN 'noirq'
+			WHEN lh.lastPreemptCount IS NULL THEN 'nolock'
+			ELSE 'unknown'
+			END
+			ORDER BY lh.start
+			SEPARATOR ' -> '
+		) AS contexts, GROUP_CONCAT(lh.lastPreemptCount ORDER BY lh.start SEPARATOR ' -> ') AS preemptCounts
+	FROM
+	(
+		-- Get all accesses. Add information about the accessed member, data type, and the function the memory has been accessed from.
+		-- Filter out every function that is on our function blacklist.
+		SELECT
+			ac.id AS ac_id,
+			ac.txn_id AS ac_txn_id,
+			ac.alloc_id AS alloc_id,
+			ac.type AS ac_type,
+			ac.fn AS ac_fn,
+			ac.address AS ac_address,
+			a.ptr AS a_ptr,
+			ac.instrptr AS ac_instrptr,
+			mn.name AS sl_member,
+			dt.name AS dt_name
+		FROM accesses AS ac
+		INNER JOIN allocations AS a ON a.id=ac.alloc_id
+		INNER JOIN data_types AS dt ON dt.id=a.type
+		LEFT JOIN structs_layout_flat sl
+		  ON a.type = sl.type_id
+		 AND ac.address - a.ptr = sl.helper_offset
+		LEFT JOIN member_names AS mn ON mn.id = sl.member_id
+		LEFT JOIN function_blacklist fn_bl
+		  ON fn_bl.datatype_id = a.type
+		 AND fn_bl.fn = ac.fn
+		 AND (fn_bl.datatype_member_id IS NULL OR fn_bl.datatype_member_id = sl.member_id)
+		WHERE 
+			-- Name the data type of interest here
+			a.type in (SELECT id FROM data_types WHERE name in ('journal_t','transaction_t')) AND
+			ac.type  IN ('r') AND -- Filter by access type
+			sl.member_id IN (SELECT id FROM member_names WHERE name in ('j_barrier_count','j_running_transaction','t_reserved_list')) AND -- Only show results for a certain member
+			fn_bl.fn IS NULL
+		GROUP BY ac.id -- Remove duplicate entries. Some accesses might be mapped to more than one member, e.g., an union.
+	) s
+	LEFT JOIN locks_held AS lh
+	  ON lh.txn_id=ac_txn_id
+	LEFT JOIN locks AS l
+	  ON l.id=lh.lock_id
+	LEFT JOIN allocations AS lock_a
+	  ON lock_a.id=l.embedded_in
+	LEFT JOIN data_types lock_a_dt
+	  ON lock_a.type = lock_a_dt.id
+	LEFT JOIN structs_layout_flat lock_member
+	  ON lock_a.type = lock_member.type_id
+	  AND l.ptr - lock_a.ptr = lock_member.helper_offset
+	JOIN member_names mn_lock_member
+	  ON mn_lock_member.id = lock_member.member_id
+	GROUP BY ac_id
+) t
 -- Since we want a detailed view about where an access happenend, the result is additionally grouped by ac_fn and ac_instrptr.
-GROUP BY ac_type, sl_member, lock_member, ac_fn, ac_instrptr
-ORDER BY ac_type, sl_member, lock_member, ac_fn, ac_instrptr, num DESC;
+GROUP BY ac_type, sl_member, locks_held, ac_fn, ac_instrptr
+ORDER BY ac_type, sl_member, locks_held, ac_fn, ac_instrptr, num DESC;
