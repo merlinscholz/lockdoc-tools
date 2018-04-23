0a0da4490228b42b0ebbc3f7958fdaa8730f069c
Alexander Lochmann <alexander.lochmann@tu-dortmund.de>
2018-04-23 16:36:17 +0200

    pretty-print-cex.py: Filter CEX by global fn blacklist, and by init or destroy paths

--- processing/pretty-print-cex.py
+++ processing/pretty-print-cex.py
@@ -17,6 +17,17 @@ import subprocess
 logging.basicConfig()
 LOGGER = logging.getLogger(__name__)
 
+# Temproray workaround to filter cex that are on an init or destroy path
+# Some day, this array will be merged into function_blacklist.csv.
+globalFnBlacklist = [ "atomic_read", "atomic_set", "atomic_add", "atomic_sub", "atomic_sub_return", "atomic_sub_and_test", "atomic_inc", "atomic_dec",
+					  "atomic_dec_return", "atomic_dec_and_test", "atomic_inc", "atomic_inc_return", "atomic_inc_and_test",
+					  "atomic_add_negative", "atomic_add_return", ",atomic_sub_return", "atomic_fetch_add", "atomic_fetch_sub",
+					  "atomic_cmpxchg", "atomic_xchg", "atomic_fetch_and", "atomic_fetch_or", "atomic_fetch_xor", "atomic_inc_short",
+					  "__atomic_add_unsless", "set_bit", "__set_bit", "clear_bit", "clear_bit_unlock", "__clear_bit", "clear_bit_unlock_is_negative_byte",
+					  "__clear_bit_unlock", "__change_bit", "change_bit", "test_and_set_bit", "test_and_set_bit_lock", "__test_and_set_bit",
+					  "test_and_clear_bit", "__test_and_clear_bit", "test_and_change_bit", "__test_and_change_bit", "constant_test_bit", "variable_test_bit",
+					  "mount_fs"]
+
 # Cache resolved instruction pointers
 addressCache = dict()
 
@@ -49,6 +60,7 @@ if __name__ == '__main__':
 	parser.add_argument('cexcsv', help='Input file containing the ground truth')
 	parser.add_argument('vmlinux', help='VMLINUX')
 	parser.add_argument('hypothesescsv', help='Input file containing preditions made by the hypothesizer (winner hypotheses only)')
+	parser.add_argument('fnblacklistcsv', help='CSV with blacklisted functions indexed by data type')
 	args = parser.parse_args()
 
 	cexCSV = args.cexcsv
@@ -56,6 +68,7 @@ if __name__ == '__main__':
 	kernelDir = args.kerneldir
 	crossRefURL = args.crossrefurl
 	hypothesesCSV = args.hypothesescsv
+	fnblacklistCSV = args.fnblacklistcsv
 	separator = ';'
 	count = 0
 	cexDict = dict()
@@ -67,7 +80,7 @@ if __name__ == '__main__':
 
 	# Parse the hypothesizer results, and store them in one large dictionary
 	tempFile = open(hypothesesCSV,'rb')
-	tempReader = csv.DictReader(tempFile, delimiter=';')	
+	tempReader = csv.DictReader(tempFile, delimiter=';')
 	for line in tempReader:
 		count = count + 1
 		key = (line['type'], line['member'], line['accesstype'])
@@ -90,6 +103,31 @@ if __name__ == '__main__':
 		else:
 			LOGGER.error('Lock combination (%s) does already exist for key %s', line['locks'], key)
 	LOGGER.debug('Read %d locking predictions for %d different (struct,member,accesstype) tuples from "%s"', count, len(hypothesesDict), hypothesesCSV)
+	tempFile.close()
+
+	# Read and parse the function blacklist
+	# $data_type --> fn: "list of blacklisted functions"
+	#				 members: $member --> fn: "list of blacklisted functions"
+	fnBlacklistDict = dict()
+	tempFile = open(fnblacklistCSV,'rb')
+	tempReader = csv.DictReader(tempFile, delimiter=';')
+	for line in tempReader:
+		count = count + 1
+		key = (line['datatype'])
+		if key not in fnBlacklistDict:
+			fnBlacklistEntry = {'members': dict(), 'fn': list()}
+			fnBlacklistDict[key] = fnBlacklistEntry
+		else:
+			fnBlacklistEntry = fnBlacklistDict[key]
+		if line['datatype_member'] == '\\N':
+			fnBlacklistEntry['fn'].append(line['fn'])
+		else:
+			if line['datatype_member'] not in fnBlacklistEntry['members']:
+				memberBlacklistEntry = list()
+				fnBlacklistEntry['members'][line['datatype_member']] = memberBlacklistEntry
+			else:
+				memberBlacklistEntry = fnBlacklistEntry['members'][line['datatype_member']]
+			memberBlacklistEntry.append(line['fn'])
 	tempFile.close()
 
 	tempFile = open(cexCSV,'rb')
@@ -199,6 +237,57 @@ tr.line_heading {
 	cexID = 1
 	hypothesisID = 0
 	for line in tempReader:
+		# Translate the stacktrace to code positions
+		# Since we want to filter out all cexs which
+		# happenend on an init/free path or use atomic funtions,
+		# we must do this so early. Otherwise, this script would
+		# have produced output.
+		traceElems = line['stacktrace'].split(',')
+		# Insert the instruction pointer
+		traceElems.insert(0, line['instrptr'])
+		# Since we want a top-down view, we reverse the stacktrace.
+		traceElems.reverse()
+		traceElemsLen = len(traceElems)
+		LOGGER.debug("%s,%s", line['instrptr'], line['stacktrace'])
+		# Convert the stacktrace into humanreadable information
+		i = 0
+		formattedStacktrace = ""
+		abort = None
+		for traceElem in traceElems:
+			codePos = addrToFn(vmlinux, traceElem, kernelDir)
+			# Is thise function globally blacklisted?
+			if codePos['fn'] in globalFnBlacklist:
+				abort = codePos['fn']
+				break
+			# Do we have blacklisted functions for this data type?
+			elif line['data_type'] in fnBlacklistDict:
+					fnBlacklistEntry = fnBlacklistDict[line['data_type']]
+					# Do we have 'global' (for all members) blacklisted functions?
+					if codePos['fn'] in fnBlacklistEntry['fn']:
+						abort = codePos['fn']
+						break
+					# We don't. But we may have member-specific blacklisted functions.
+					elif line['member'] in fnBlacklistEntry['members']:
+						fnBlMemberEntry = fnBlacklistEntry['members']
+						if codePos['fn'] in fnBlMemberEntry['fn']:
+							abort = codePos['fn']
+							break
+			# Mark the first stacktrace entry since it corresponds
+			# to the suspicious memory access.
+			if i == (traceElemsLen - 1):
+				formattedStacktrace = formattedStacktrace + '</div>\n'
+				formattedStacktrace = formattedStacktrace + '%02d: <a class="memaccess" ' % (traceElemsLen - (i + 1))
+			else:
+				formattedStacktrace = formattedStacktrace + '%02d: <a class="stacktraceelem" ' % (traceElemsLen - (i + 1))
+			formattedStacktrace = formattedStacktrace + 'href="%s/source/%s#L%s">%s:%s</a>' % (crossRefURL, codePos['file'], codePos['line'], codePos['fn'], codePos['line'])
+			if i < (traceElemsLen - 1):
+				formattedStacktrace = formattedStacktrace + '<br/>\n'
+			i=i+1
+		# Abort further processing of this cex, it isn't a real one.
+		if abort is not None:
+			LOGGER.debug('Filtered cex for (%s,%s,%s) due to blacklisted function: %s.' % (line['data_type'], line['member'], line['accesstype'], codePos['fn']))
+			continue
+		
 		key = (line['data_type'], line['member'], line['accesstype'])
 		if key not in hypothesesDict:
 			LOGGER.error('Key %s does not exist in %s' %(key, hypothesesCSV))
@@ -248,30 +337,7 @@ tr.line_heading {
 				i = i + 1
 		else:
 			print('No locks')
-		print('</td><td>', end='')
-		traceElems = line['stacktrace'].split(',')
-		# Insert the instruction pointer
-		traceElems.insert(0, line['instrptr'])
-		# Since we want a top-down view, we reverse the stacktrace.
-		traceElems.reverse()
-		traceElemsLen = len(traceElems)
-		# Convert the stacktrace into humanreadable information
-		i = 0
-		print('<button class="accordion">Stacktrace</button>')
-		print('<div class="stacktrace">')
-		for traceElem in traceElems:
-			codePos = addrToFn(vmlinux, traceElem, kernelDir)
-			# Mark the first stacktrace entry since it corresponds
-			# to the suspicious memory access.
-			if i == (traceElemsLen - 1):
-				print('</div>')
-				print('%02d: <a class="memaccess" ' % (traceElemsLen - (i + 1)), end='')
-			else:
-				print('%02d: <a class="stacktraceelem" ' % (traceElemsLen - (i + 1)), end='')
-			print('href="%s/source/%s#L%s">%s:%s</a>' %(crossRefURL, codePos['file'], codePos['line'], codePos['fn'], codePos['line']), end='')
-			if i < (traceElemsLen - 1):
-				print('<br/>', end='')
-			i=i+1
+		print('</td><td><button class="accordion">Stacktrace</button>\n<div class="stacktrace">%s</td>' % (formattedStacktrace))
 		print('</td>')
 		print('		</tr>')
 		lineCounter = lineCounter + 1
