1174785a0cdc69ea42ac1fa4361ed66465ce2391
Alexander Lochmann <alexander.lochmann@tu-dortmund.de>
2018-06-01 12:37:40 +0200

    misc.sql: Get accesses that would have been ignored

--- queries/misc.sql
+++ queries/misc.sql
@@ -30,3 +30,41 @@ FROM accesses As ac
 LEFT JOIN locks_held AS lh ON lh.access_id=ac.id
 INNER JOIN allocations AS a ON a.id=ac.alloc_id
 WHERE lh.start IS NULL -- AND a.id = (SELECT id FROM data_types WHERE name = 'inode');
+
+-- Get accesses to $data_type.$member which would have been ignored
+SELECT ac.id, ac.type AS type, a.type AS type_id, mn.name AS member, sl.offset,
+	sl.size, st.sequence, st.function, m_bl.datatype_member_id IS NOT NULL AS member_bl, fn_bl.fn IS NOT NULL AS fn_bl
+FROM accesses ac
+JOIN allocations a
+  ON ac.alloc_id = a.id
+JOIN stacktraces AS st
+  ON ac.stacktrace_id = st.id
+-- AND st.sequence = 0
+LEFT JOIN structs_layout_flat sl
+  ON a.type = sl.type_id
+ AND ac.address - a.ptr = sl.helper_offset
+LEFT JOIN member_names mn
+  ON mn.id = sl.member_id
+LEFT JOIN member_blacklist m_bl
+  ON m_bl.datatype_id = a.type
+ AND m_bl.datatype_member_id = sl.member_id
+LEFT JOIN function_blacklist fn_bl
+  ON fn_bl.fn = st.function
+ AND 
+ (
+   (fn_bl.data_type_id IS NULL  AND fn_bl.member_name_id IS NULL) -- globally blacklisted function
+   OR
+   (fn_bl.data_type_id = a.type AND fn_bl.member_name_id IS NULL) -- for this data type blacklisted
+   OR
+   (fn_bl.data_type_id = a.type AND fn_bl.member_name_id = sl.member_id) -- for this member blacklisted
+ )
+WHERE 1
+AND a.type IN (SELECT id FROM data_types WHERE name = 'super_block')
+AND mn.id IN (SELECT id FROM member_names WHERE name = 's_writers' OR name = 's_inode_list_lock')
+-- === FOR NOW: skip task_struct ===
+AND a.type != (SELECT id FROM data_types WHERE name = 'task_struct')
+-- ====================================
+AND 
+(
+	fn_bl.fn IS NOT NULL OR m_bl.datatype_member_id IS NOT NULL
+)
