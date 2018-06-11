562ef7bdff215e6d41167d9b0f033a5395725ce7
Alexander Lochmann <alexander.lochmann@tu-dortmund.de>
2018-06-11 11:57:07 +0200

    CEX: Group cex view by stacktrace

--- processing/pretty-print-cex.py
+++ processing/pretty-print-cex.py
@@ -164,20 +164,44 @@ tr.line_heading {
 	<h2>Results</h2>
 	<table>
 		<tr>
-			<!--<th>Data Type</th><th>Member</th><th>Access Type</th>--><th>ID</th><th>Occurrences</th><th>Locks Held<br/>(first&nbsp;&rarr;&nbsp;most recent)</th><th>Stacktrace<br/>(top-down)</th>
+			<th>Stacktrace<br/>(00 = stackframe where access occurred)</th><th>ID</th><th>Occurrences</th><th><span style="color:red">Locks actually held<br/>(in order locks were taken)</span></th>
 
 		</tr>""")
-	
-	lineCounter = 0
 	lastKey = None
 	cexID = 1
 	hypothesisID = 0
+	outerLineStyle = 'a'
 	for line in tempReader:
-		# Translate the stacktrace to code positions
-		# Since we want to filter out all cexs which
-		# happenend on an init/free path or use atomic funtions,
-		# we must do this so early. Otherwise, this script would
-		# have produced output.
+		key = (line['data_type'], line['member'], line['accesstype'])
+		if key not in hypothesesDict:
+			LOGGER.error('Key %s does not exist in %s' %(key, hypothesesCSV))
+			continue
+		else:
+			hypothesesEntry = hypothesesDict[key]
+		# Print a header for each tuple of (data_type, member, accesstype)
+		if lastKey != key:
+			hypothesisID = hypothesisID + 1
+			print('		<tr class="line_heading">')
+			# The header contains information about the accessed member like
+			# the access type or the locking rule.
+			# Moreover, it shows statistics about the locking rule, e.g., the fraction ('percentage') of all accesses ('total') that adhere to that rule.
+			# E.g.: 94.6% (19069 out of 20155 mem accesses under locks): EMBOTHER(inode:i_rwsem)
+			locksHeldKey = hypothesesEntry['locks'].keys()[0]
+			locksHeldEntry = hypothesesEntry['locks'][locksHeldKey]
+			print('				<td colspan="5"><b>Hypothesis %d</b>: When <b>%s %s.%s</b> the following locks <span style="color:green;font-weight:bold;">should be held</span>: <span style="font-weight:bold;color:blue;">%s</span><br/>'
+				% (hypothesisID, 'reading' if line['accesstype'] == 'r' else 'writing', line['data_type'], line['member'], locksHeldKey), end='')
+			print('<b>%2.2f%%</b> (%d out of %d mem accesses under locks)</td>' % (locksHeldEntry['percentage'], locksHeldEntry['occurrences'], locksHeldEntry['total']))
+			print('		</tr>')
+			cexID = 1
+		lastKey = key
+
+		# Example:
+		# EMBSAME(j_barrier)@jbd2_journal_lock_updates@fs/jbd2/transaction.c:746#1+EMBSAME(j_barrier)@jbd2_journal_lock_updates@fs/jbd2/transaction.c:746#1
+		lockCombinations = line['locks_held'].split('+')
+		print('		<tr class="line_%s">' % ('a' if outerLineStyle == 'a' else 'b'))
+		#print('			<td rowspan="%d"><button class="accordion">Stacktrace</button>\n\t\t\t\t<div class="stacktrace">\n' % (len(lockCombinations)), end='')
+		print('			<td rowspan="%d">' % (len(lockCombinations)), end='')
+		# Extract stacktrace
 		traceElems = line['stacktrace'].split(',')
 		# Since we want a top-down view, we reverse the stacktrace.
 		traceElems.reverse()
@@ -197,69 +221,71 @@ tr.line_heading {
 			# Mark the first stacktrace entry since it corresponds
 			# to the suspicious memory access.
 			if i == (traceElemsLen - 1):
-				formattedStacktrace = formattedStacktrace + '</div>\n'
-				formattedStacktrace = formattedStacktrace + '%02d: <a class="memaccess" ' % (traceElemsLen - (i + 1))
+				#print('				</div>\n', end='')
+				print('					%02d: <a class="memaccess" ' % (traceElemsLen - (i + 1)), end='')
 			else:
-				formattedStacktrace = formattedStacktrace + '%02d: <a class="stacktraceelem" ' % (traceElemsLen - (i + 1))
-			formattedStacktrace = formattedStacktrace + 'href="%s/source/%s#L%s">%s:%s</a>' % (crossRefURL, codePos['file'], codePos['line'], codePos['fn'], codePos['line'])
+				print('					%02d: <a class="stacktraceelem" ' % (traceElemsLen - (i + 1)), end='')
+			print('href="%s/source/%s#L%s">%s:%s</a>' % (crossRefURL, codePos['file'], codePos['line'], codePos['fn'], codePos['line']), end='')
 			if i < (traceElemsLen - 1):
-				formattedStacktrace = formattedStacktrace + '<br/>\n'
+				print('<br/>\n', end='')
 			i=i+1
-		
-		key = (line['data_type'], line['member'], line['accesstype'])
-		if key not in hypothesesDict:
-			LOGGER.error('Key %s does not exist in %s' %(key, hypothesesCSV))
-			continue
+		print('\n			</td>')
+
+		# Set the inner line style
+		if outerLineStyle == 'a':
+			innerLineStyle = 'b'
 		else:
-			hypothesesEntry = hypothesesDict[key]
-		# Print a header for each tuple of (data_type, member, accesstype)
-		if lastKey != key:
-			hypothesisID = hypothesisID + 1
-			print('		<tr class="line_heading">')
-			# The header contains information about the accessed member like
-			# the access type or the locking rule.
-			# Moreover, it shows statistics about the locking rule, e.g., the fraction ('percentage') of all accesses ('total') that adhere to that rule.
-			# E.g.: 94.6% (19069 out of 20155 mem accesses under locks): EMBOTHER(inode:i_rwsem)			
-			locksHeldKey = hypothesesEntry['locks'].keys()[0]
-			locksHeldEntry = hypothesesEntry['locks'][locksHeldKey]
-			print('<td colspan="5"><b>Hypothesis %d</b>: <b>%s</b> access to <b>%s.%s</b> is proteced by <b>%s</b><br/>' % (hypothesisID, 'Read' if line['accesstype'] == 'r' else 'Write', line['data_type'], line['member'], locksHeldKey), end='')
-			print('<b>%2.2f%%</b> (%d out of %d mem accesses under locks)</td>' % (locksHeldEntry['percentage'], locksHeldEntry['occurrences'], locksHeldEntry['total']))
+			innerLineStyle = 'a'
+		# Switch the outer line style *after* the inner line style was set.
+		# Otherwise two adjacent rows will have the same color
+		if outerLineStyle == 'a':
+			outerLineStyle = 'b'
+		else:
+			outerLineStyle = 'a'
+
+		i = 0
+		# $locks_held#occurrences
+		# Example:
+		# EMBSAME(j_barrier)@jbd2_journal_lock_updates@fs/jbd2/transaction.c:746#1
+		for lockComb in lockCombinations:
+			locksHeld = lockComb.split('#')[0]
+			occurences = lockComb.split('#')[1]
+
+			if i > 0:
+				print('		<tr class="line_%s">' % ('a' if innerLineStyle == 'a' else 'b'))
+				# Alternate the inner line style if it is the second row or any row later.
+				# The first row has the same style as the stacktrace row.
+				if innerLineStyle == 'a':
+					innerLineStyle = 'b'
+				else:
+					innerLineStyle = 'a'
+			print('			<td>%d.%d</td><td>%s</td>' % (hypothesisID, cexID, occurences), end='')
+			print('<td>', end='')
+			# Split locks_held
+			# Example: EMBSAME(j_barrier)@jbd2_journal_lock_updates@fs/jbd2/transaction.c:746, EMBSAME(j_state_lo0ck)@jbd2_journal_lock_updates@fs/jbd2/transaction.c:42
+			if locksHeld != "nolocks":
+				locksHeld = locksHeld.split(',')
+				locksHeldLen = len(locksHeld)
+				k = 0
+				# Example: EMBSAME(j_barrier)@jbd2_journal_lock_updates@fs/jbd2/transaction.c:746
+				for lockHeld in locksHeld:
+					# Index 0: the lock
+					# Index 1: the function where the lock was acquired
+					# Index 2: the file and line where the lock was acquired
+					elems = lockHeld.split('@')
+					lockFile = elems[2].split(':')[0]
+					lockLine = elems[2].split(':')[1] #
+					print('%02d: <a class="lock" href="%s/source/%s#L%s" title="%s@%s:%s">%s</a>' 
+						%(k + 1, crossRefURL, lockFile, lockLine, elems[1], lockFile, lockLine, elems[0]), end='')
+					if k < (locksHeldLen - 1):
+						print('<br/>', end='')
+					k = k + 1
+			else:
+				print('No locks')
+			print('</td>')
 			print('		</tr>')
-			cexID = 1
-		lastKey = key
-		
-		if lineCounter % 2 == 0:
-			lineStyle = 'class="line_a"'
-		else:
-			lineStyle = 'class="line_b"'
-		print('		<tr %s>' % lineStyle)
-		print('			<!--<td>%s</td><td>%s</td><td>%s</td>--><td>%d.%d</td><td>%s</td>' %(line['data_type'], line['member'], line['accesstype'], hypothesisID, cexID, line['occurrences']), end='')
-		print('<td>', end='')
-		
-		# Split locks_held
-		# Example: EMBSAME(j_barrier)@jbd2_journal_lock_updates@fs/jbd2/transaction.c:746
-		if line['locks_held'] != "NULL":
-			locksHeld = line['locks_held'].split(',')
-			locksHeldLen = len(locksHeld)
-			i = 0
-			for lockHeld in locksHeld:
-				# Index 0: the lock
-				# Index 1: the function where the lock was acquired
-				# Index 2: the file and line where the lock was acquired
-				elems = lockHeld.split('@')
-				lockFile = elems[2].split(':')[0]
-				lockLine = elems[2].split(':')[1]
-				print('%02d: <a class="lock" href="%s/source/%s#L%s" title="%s@%s:%s">%s</a>' %(i + 1, crossRefURL, lockFile, lockLine, elems[1], lockFile, lockLine, elems[0]), end='')
-				if i < (locksHeldLen - 1):
-					print('<br/>', end='')
-				i = i + 1
-		else:
-			print('No locks')
-		print('</td><td><button class="accordion">Stacktrace</button>\n<div class="stacktrace">%s</td>' % (formattedStacktrace))
-		print('</td>')
-		print('		</tr>')
-		lineCounter = lineCounter + 1
-		cexID = cexID + 1
+			cexID = cexID + 1
+			i = i + 1
 
 	print("""	</table>
 <script type='text/javascript'>
--- queries/counterexample.sql.sh
+++ queries/counterexample.sql.sh
@@ -58,69 +58,44 @@ fi
 
 cat <<EOT
 SET SESSION group_concat_max_len = 100000;
-SELECT '${DATATYPE}' AS data_type, '${MEMBER}' AS member, '${ACCESSTYPE}' AS accesstype, COUNT(*) AS occurrences, stacktrace, locks_held
+SELECT data_type, member, accesstype, stacktrace, 
+	GROUP_CONCAT(
+		CONCAT(locks_held, '#', occurrences)
+		ORDER BY occurrences DESC, locks_held
+		SEPARATOR '+'
+	) AS locks_held
 FROM
 (
-	SELECT stacktrace_id,
-		GROUP_CONCAT(
-			stacktrace_elem
-			ORDER BY st_sequence
-			SEPARATOR ','
-		) AS stacktrace, locks_held
-	
+	SELECT '${DATATYPE}' AS data_type, '${MEMBER}' AS member, '${ACCESSTYPE}' AS accesstype, COUNT(*) AS occurrences, stacktrace_id, stacktrace, locks_held
 	FROM
 	(
-		SELECT ac.id AS ac_id, stacktrace_id, CONCAT('0x', HEX(st.instruction_ptr), '@', st.function, '@', st.file, ':', st.line) AS stacktrace_elem, st.sequence AS st_sequence,
-		GROUP_CONCAT(
-			CASE
-			WHEN l.embedded_in IS NULL AND l.lock_var_name IS NULL
-				THEN CONCAT(l.id, '(', l.type, '[', l.sub_lock, '])', '@', lh.lastFn, '@', lh.lastFile, ':', lh.lastLine) -- global (or embedded in unknown allocation *and* no name available)
-			WHEN l.embedded_in IS NULL AND l.lock_var_name IS NOT NULL
-				THEN CONCAT(l.lock_var_name, ':', l.id, '(', l.type, '[', l.sub_lock, '])', '@', lh.lastFn, '@', lh.lastFile, ':', lh.lastLine) -- global (or embedded in unknown allocation *and* a name is available)
-			WHEN l.embedded_in IS NOT NULL AND l.embedded_in = ac.alloc_id
-				THEN CONCAT('EMBSAME(', IF(l.ptr - lock_a.ptr = lock_member.offset, lock_member_name.name, CONCAT(lock_member_name.name, '?')), '[', l.sub_lock, '])', '@', lh.lastFn, '@', lh.lastFile, ':', lh.lastLine) -- embedded in same
-				${EMBOTHER_SQL}
-			END
-			ORDER BY lh.start
-			SEPARATOR ','
-		) AS locks_held
-
+		SELECT stacktrace_id,
+			GROUP_CONCAT(
+				stacktrace_elem
+				ORDER BY st_sequence
+				SEPARATOR ','
+			) AS stacktrace, IF(locks_held IS NULL, 'nolocks', locks_held) AS locks_held
+		
 		FROM
 		(
-			SELECT ac.id, ac.txn_id, ac.alloc_id, st.function, ac.stacktrace_id
-			FROM accesses ac
-			JOIN allocations a
-			  ON ac.alloc_id = a.id
-			 AND ac.type = '$ACCESSTYPE'
-			JOIN data_types dt
-			  ON a.type = dt.id
-			 AND dt.name = '$DATATYPE'
-			JOIN stacktraces AS st
-			  ON ac.stacktrace_id = st.id
-			 AND st.sequence = 0
-			JOIN structs_layout_flat sl
-			  ON sl.type_id = a.type
-			 AND sl.helper_offset = ac.address - a.ptr
-			JOIN member_names mn
-			  ON mn.id = sl.member_id
-			 AND mn.name = '$MEMBER'
-			WHERE
-EOT
-if [ $MODE = CEX ]; then
-	cat <<EOT
-			ac.id NOT IN	-- counterexample
-EOT
-elif [ $MODE = EX ]; then
-	cat <<EOT
-			ac.id IN		-- example
-EOT
-else
-	echo "error: unknown MODE $MODE" >&2
-	exit 1
-fi
-cat <<EOT
+			SELECT ac.id AS ac_id, stacktrace_id, CONCAT('0x', HEX(st.instruction_ptr), '@', st.function, '@', st.file, ':', st.line) AS stacktrace_elem, st.sequence AS st_sequence,
+			GROUP_CONCAT(
+				CASE
+				WHEN l.embedded_in IS NULL AND l.lock_var_name IS NULL
+					THEN CONCAT(l.id, '(', l.type, '[', l.sub_lock, '])', '@', lh.lastFn, '@', lh.lastFile, ':', lh.lastLine) -- global (or embedded in unknown allocation *and* no name available)
+				WHEN l.embedded_in IS NULL AND l.lock_var_name IS NOT NULL
+					THEN CONCAT(l.lock_var_name, ':', l.id, '(', l.type, '[', l.sub_lock, '])', '@', lh.lastFn, '@', lh.lastFile, ':', lh.lastLine) -- global (or embedded in unknown allocation *and* a name is available)
+				WHEN l.embedded_in IS NOT NULL AND l.embedded_in = ac.alloc_id
+					THEN CONCAT('EMBSAME(', IF(l.ptr - lock_a.ptr = lock_member.offset, lock_member_name.name, CONCAT(lock_member_name.name, '?')), '[', l.sub_lock, '])', '@', lh.lastFn, '@', lh.lastFile, ':', lh.lastLine) -- embedded in same
+					${EMBOTHER_SQL}
+				END
+				ORDER BY lh.start
+				SEPARATOR ','
+			) AS locks_held
+
+			FROM
 			(
-				SELECT ac.id
+				SELECT ac.id, ac.txn_id, ac.alloc_id, st.function, ac.stacktrace_id
 				FROM accesses ac
 				JOIN allocations a
 				  ON ac.alloc_id = a.id
@@ -128,12 +103,45 @@ cat <<EOT
 				JOIN data_types dt
 				  ON a.type = dt.id
 				 AND dt.name = '$DATATYPE'
+				JOIN stacktraces AS st
+				  ON ac.stacktrace_id = st.id
+				 AND st.sequence = 0
 				JOIN structs_layout_flat sl
 				  ON sl.type_id = a.type
 				 AND sl.helper_offset = ac.address - a.ptr
 				JOIN member_names mn
 				  ON mn.id = sl.member_id
 				 AND mn.name = '$MEMBER'
+				WHERE
+EOT
+if [ $MODE = CEX ]; then
+	cat <<EOT
+				ac.id NOT IN	-- counterexample
+EOT
+elif [ $MODE = EX ]; then
+	cat <<EOT
+				ac.id IN		-- example
+EOT
+else
+	echo "error: unknown MODE $MODE" >&2
+	exit 1
+fi
+cat <<EOT
+				(
+					SELECT ac.id
+					FROM accesses ac
+					JOIN allocations a
+					  ON ac.alloc_id = a.id
+					 AND ac.type = '$ACCESSTYPE'
+					JOIN data_types dt
+					  ON a.type = dt.id
+					 AND dt.name = '$DATATYPE'
+					JOIN structs_layout_flat sl
+					  ON sl.type_id = a.type
+					 AND sl.helper_offset = ac.address - a.ptr
+					JOIN member_names mn
+					  ON mn.id = sl.member_id
+					 AND mn.name = '$MEMBER'
 EOT
 
 LOCKNR=1
@@ -148,33 +156,33 @@ for LOCK in "$@"; do
 			exit 1
 		fi
 cat <<EOT
-				-- lock #$LOCKNR
-				JOIN locks_held lh_sbh${LOCKNR} -- sbh = ShouldBeHeld
-				  ON lh_sbh${LOCKNR}.txn_id = ac.txn_id
-				JOIN locks l_sbh${LOCKNR}
-				  ON l_sbh${LOCKNR}.id = lh_sbh${LOCKNR}.lock_id
-				 AND l_sbh${LOCKNR}.sub_lock = '${SUBLOCK}'
-				JOIN allocations l_sbh_a${LOCKNR}
-				  ON l_sbh${LOCKNR}.embedded_in = l_sbh_a${LOCKNR}.id
-				JOIN structs_layout_flat lock_member_sbh${LOCKNR}
-				  ON l_sbh_a${LOCKNR}.type = lock_member_sbh${LOCKNR}.type_id
-				 AND l_sbh${LOCKNR}.ptr - l_sbh_a${LOCKNR}.ptr = lock_member_sbh${LOCKNR}.helper_offset
-				JOIN member_names lock_member_name_sbh${LOCKNR}
-				  ON lock_member_name_sbh${LOCKNR}.id = lock_member_sbh${LOCKNR}.member_id
-				 AND lock_member_name_sbh${LOCKNR}.name = '$LOCKNAME'
+					-- lock #$LOCKNR
+					JOIN locks_held lh_sbh${LOCKNR} -- sbh = ShouldBeHeld
+					  ON lh_sbh${LOCKNR}.txn_id = ac.txn_id
+					JOIN locks l_sbh${LOCKNR}
+					  ON l_sbh${LOCKNR}.id = lh_sbh${LOCKNR}.lock_id
+					 AND l_sbh${LOCKNR}.sub_lock = '${SUBLOCK}'
+					JOIN allocations l_sbh_a${LOCKNR}
+					  ON l_sbh${LOCKNR}.embedded_in = l_sbh_a${LOCKNR}.id
+					JOIN structs_layout_flat lock_member_sbh${LOCKNR}
+					  ON l_sbh_a${LOCKNR}.type = lock_member_sbh${LOCKNR}.type_id
+					 AND l_sbh${LOCKNR}.ptr - l_sbh_a${LOCKNR}.ptr = lock_member_sbh${LOCKNR}.helper_offset
+					JOIN member_names lock_member_name_sbh${LOCKNR}
+					  ON lock_member_name_sbh${LOCKNR}.id = lock_member_sbh${LOCKNR}.member_id
+					 AND lock_member_name_sbh${LOCKNR}.name = '$LOCKNAME'
 EOT
 
 	elif echo $LOCK | grep -q '^\(EMB:\|[A-Za-z_]\+:\)\?[0-9]\+('; then # e.g., EMB:123(i_mutex) or 34(spinlock_t), or console_sem:4711(mutex)
 		LOCKID=$(echo $LOCK | sed -e 's/^\(EMB:\|[A-Za-z_]\+:\)\?\([0-9]\+\)(.*\[\([rw]\)\])$/\2/') # 2st numeric sequence in $LOCK
 		SUBLOCK=$(echo $LOCK | sed -e 's/^\(EMB:\|[A-Za-z_]\+:\)\?\([0-9]\+\)(.*\[\([rw]\)\])$/\3/')
 		cat <<EOT
-				-- lock #$LOCKNR
-				JOIN locks_held lh_sbh${LOCKNR}
-				  ON lh_sbh${LOCKNR}.txn_id = ac.txn_id
-				 AND lh_sbh${LOCKNR}.lock_id = $LOCKID
-				JOIN locks l_sbh${LOCKNR}
-				  ON l_sbh${LOCKNR}.id = lh_sbh${LOCKNR}.lock_id
-				 AND l_sbh${LOCKNR}.sub_lock = '${SUBLOCK}'
+					-- lock #$LOCKNR
+					JOIN locks_held lh_sbh${LOCKNR}
+					  ON lh_sbh${LOCKNR}.txn_id = ac.txn_id
+					 AND lh_sbh${LOCKNR}.lock_id = $LOCKID
+					JOIN locks l_sbh${LOCKNR}
+					  ON l_sbh${LOCKNR}.id = lh_sbh${LOCKNR}.lock_id
+					 AND l_sbh${LOCKNR}.sub_lock = '${SUBLOCK}'
 EOT
 	elif echo $LOCK | grep -q '^EMBOTHER'; then # e.g., EMBOTHER(i_mutex)
 #		LOCKNAME=$(echo $LOCK | sed -e 's/^.*(\(.*\))$/\1/')
@@ -185,25 +193,25 @@ EOT
 			exit 1
 		fi
 cat <<EOT
-				-- lock #$LOCKNR
-				JOIN locks_held lh_sbh${LOCKNR} -- sbh = ShouldBeHeld
-				  ON lh_sbh${LOCKNR}.txn_id = ac.txn_id
-				JOIN locks l_sbh${LOCKNR}
-				  ON l_sbh${LOCKNR}.id = lh_sbh${LOCKNR}.lock_id
-				 AND l_sbh${LOCKNR}.embedded_in != a.id
-				 AND l_sbh${LOCKNR}.sub_lock = '${SUBLOCK}'
-				JOIN allocations l_sbh_a${LOCKNR}
-				  ON l_sbh${LOCKNR}.embedded_in = l_sbh_a${LOCKNR}.id
-				JOIN structs_layout_flat lock_member_sbh${LOCKNR}
-				  ON l_sbh_a${LOCKNR}.type = lock_member_sbh${LOCKNR}.type_id
-				 AND l_sbh${LOCKNR}.ptr - l_sbh_a${LOCKNR}.ptr = lock_member_sbh${LOCKNR}.helper_offset
-				JOIN member_names lock_member_name_sbh${LOCKNR}
-				  ON lock_member_name_sbh${LOCKNR}.id = lock_member_sbh${LOCKNR}.member_id
-				 AND lock_member_name_sbh${LOCKNR}.name = '$LOCKNAME'
+					-- lock #$LOCKNR
+					JOIN locks_held lh_sbh${LOCKNR} -- sbh = ShouldBeHeld
+					  ON lh_sbh${LOCKNR}.txn_id = ac.txn_id
+					JOIN locks l_sbh${LOCKNR}
+					  ON l_sbh${LOCKNR}.id = lh_sbh${LOCKNR}.lock_id
+					 AND l_sbh${LOCKNR}.embedded_in != a.id
+					 AND l_sbh${LOCKNR}.sub_lock = '${SUBLOCK}'
+					JOIN allocations l_sbh_a${LOCKNR}
+					  ON l_sbh${LOCKNR}.embedded_in = l_sbh_a${LOCKNR}.id
+					JOIN structs_layout_flat lock_member_sbh${LOCKNR}
+					  ON l_sbh_a${LOCKNR}.type = lock_member_sbh${LOCKNR}.type_id
+					 AND l_sbh${LOCKNR}.ptr - l_sbh_a${LOCKNR}.ptr = lock_member_sbh${LOCKNR}.helper_offset
+					JOIN member_names lock_member_name_sbh${LOCKNR}
+					  ON lock_member_name_sbh${LOCKNR}.id = lock_member_sbh${LOCKNR}.member_id
+					 AND lock_member_name_sbh${LOCKNR}.name = '$LOCKNAME'
 EOT
 		if [ $LAST_EMBOTHER -gt 0 ]; then
 				cat <<EOT
-				AND lh_sbh${LOCKNR}.start > lh_sbh${LAST_EMBOTHER}.start -- ensure the same EMBOTHER lock gets joined just once
+					AND lh_sbh${LOCKNR}.start > lh_sbh${LAST_EMBOTHER}.start -- ensure the same EMBOTHER lock gets joined just once
 EOT
 		fi
 		LAST_EMBOTHER=$LOCKNR
@@ -214,78 +222,80 @@ EOT
 
 	if [ $LOCKCONNECTOR = SEQ -a $LOCKNR -gt 1 ]; then
 		cat <<EOT
-				AND lh_sbh${LOCKNR}.start > lh_sbh$(($LOCKNR - 1)).start -- temporal sequence
+					AND lh_sbh${LOCKNR}.start > lh_sbh$(($LOCKNR - 1)).start -- temporal sequence
 EOT
 	fi
 
 	LOCKNR=$((LOCKNR + 1))
 done
 cat <<EOT
-			)
-			AND ac.id NOT IN
-			(
-			-- Get all accesses that happened on an init path
-				SELECT ac.id
-				FROM accesses ac
-				JOIN allocations a
-				  ON ac.alloc_id = a.id
-				 AND ac.type = '$ACCESSTYPE'
-				JOIN data_types dt
-				  ON a.type = dt.id
-				 AND dt.name = '$DATATYPE'
-				JOIN stacktraces AS st
-				  ON ac.stacktrace_id = st.id
-				JOIN structs_layout_flat sl
-				  ON sl.type_id = a.type
-				 AND sl.helper_offset = ac.address - a.ptr
-				JOIN member_names mn
-				  ON mn.id = sl.member_id
-				 AND mn.name = '$MEMBER'
-				LEFT JOIN function_blacklist fn_bl
-				  ON fn_bl.fn = st.function
-				 AND
-				 (
-				   (fn_bl.data_type_id IS NULL  AND fn_bl.member_name_id IS NULL) -- globally blacklisted function
-				   OR
-				   (fn_bl.data_type_id = a.type AND fn_bl.member_name_id IS NULL) -- for this data type blacklisted
-				   OR
-				   (fn_bl.data_type_id = a.type AND fn_bl.member_name_id = sl.member_id) -- for this member blacklisted
-				 )
-				WHERE
-					fn_bl.fn IS NOT NULL
-				GROUP BY ac.id
-			)
-		) ac
+				)
+				AND ac.id NOT IN
+				(
+				-- Get all accesses that happened on an init path
+					SELECT ac.id
+					FROM accesses ac
+					JOIN allocations a
+					  ON ac.alloc_id = a.id
+					 AND ac.type = '$ACCESSTYPE'
+					JOIN data_types dt
+					  ON a.type = dt.id
+					 AND dt.name = '$DATATYPE'
+					JOIN stacktraces AS st
+					  ON ac.stacktrace_id = st.id
+					JOIN structs_layout_flat sl
+					  ON sl.type_id = a.type
+					 AND sl.helper_offset = ac.address - a.ptr
+					JOIN member_names mn
+					  ON mn.id = sl.member_id
+					 AND mn.name = '$MEMBER'
+					LEFT JOIN function_blacklist fn_bl
+					  ON fn_bl.fn = st.function
+					 AND
+					 (
+					   (fn_bl.data_type_id IS NULL  AND fn_bl.member_name_id IS NULL) -- globally blacklisted function
+					   OR
+					   (fn_bl.data_type_id = a.type AND fn_bl.member_name_id IS NULL) -- for this data type blacklisted
+					   OR
+					   (fn_bl.data_type_id = a.type AND fn_bl.member_name_id = sl.member_id) -- for this member blacklisted
+					 )
+					WHERE
+						fn_bl.fn IS NOT NULL
+					GROUP BY ac.id
+				)
+			) ac
 
-		JOIN stacktraces AS st
-		  ON ac.stacktrace_id = st.id
+			JOIN stacktraces AS st
+			  ON ac.stacktrace_id = st.id
 
-		LEFT JOIN locks_held lh
-		  ON lh.txn_id = ac.txn_id
-		LEFT JOIN locks l
-		  ON l.id = lh.lock_id
+			LEFT JOIN locks_held lh
+			  ON lh.txn_id = ac.txn_id
+			LEFT JOIN locks l
+			  ON l.id = lh.lock_id
 
-		-- find out more about each held lock (allocation -> structs_layout
-		-- member or contained-in member in case of a complex member)
-		LEFT JOIN allocations lock_a
-		  ON l.embedded_in = lock_a.id
-		LEFT JOIN structs_layout_flat lock_member
-		  ON lock_a.type = lock_member.type_id
-		 AND l.ptr - lock_a.ptr = lock_member.helper_offset
-		LEFT JOIN member_names lock_member_name
-		  ON lock_member_name.id = lock_member.member_id
-		-- lock_a.id IS NULL                         => not embedded
-		-- l.ptr - lock_a.ptr = lock_member.offset   => the lock is exactly this member (or at the beginning of a complex sub-struct)
-		-- else                                      => the lock is contained in this member, exact name unknown
-		-- Joining the stacktraces table multiplies each row by the number of stackframes an access has.
-		-- First, (group) concat all locks held during one access, but preserve one row for each stackframe.
-		-- One might leave ac.stacktrace_id out, because each access (--> unique ac.id) has exactly one assiotciated stacktrace.
-		GROUP BY ac.id, ac.stacktrace_id, st.sequence
-	) folded_locks
-	-- Now collapse the stacktrace to one row (aka GROUP_CONCAT)
-	GROUP BY ac_id
-) all_counterexamples
-GROUP BY data_type, member, accesstype, stacktrace_id, locks_held
-ORDER BY data_type, member, accesstype, stacktrace_id, locks_held, occurrences
+			-- find out more about each held lock (allocation -> structs_layout
+			-- member or contained-in member in case of a complex member)
+			LEFT JOIN allocations lock_a
+			  ON l.embedded_in = lock_a.id
+			LEFT JOIN structs_layout_flat lock_member
+			  ON lock_a.type = lock_member.type_id
+			 AND l.ptr - lock_a.ptr = lock_member.helper_offset
+			LEFT JOIN member_names lock_member_name
+			  ON lock_member_name.id = lock_member.member_id
+			-- lock_a.id IS NULL                         => not embedded
+			-- l.ptr - lock_a.ptr = lock_member.offset   => the lock is exactly this member (or at the beginning of a complex sub-struct)
+			-- else                                      => the lock is contained in this member, exact name unknown
+			-- Joining the stacktraces table multiplies each row by the number of stackframes an access has.
+			-- First, (group) concat all locks held during one access, but preserve one row for each stackframe.
+			-- One might leave ac.stacktrace_id out, because each access (--> unique ac.id) has exactly one assiotciated stacktrace.
+			GROUP BY ac.id, ac.stacktrace_id, st.sequence
+		) folded_locks
+		-- Now collapse the stacktrace to one row (aka GROUP_CONCAT)
+		GROUP BY ac_id
+	) all_counterexamples
+	GROUP BY data_type, member, accesstype, stacktrace_id, locks_held
+) all_counterexamples_by_stack
+GROUP BY data_type, member, accesstype, stacktrace_id
+ORDER BY data_type, member, accesstype, stacktrace_id
 ;
 EOT
