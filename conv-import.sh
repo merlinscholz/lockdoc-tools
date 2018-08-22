81b68c9943c3a22b471f82d0a622c5a1b2df26d1
Alexander Lochmann <alexander.lochmann@tu-dortmund.de>
2018-08-22 10:30:05 +0200

    conv-import.sh: Var KERNEL_TREE is now mandatory

--- conv-import.sh
+++ conv-import.sh
@@ -26,9 +26,9 @@ MEMBER_BLACK_LIST Blacklisted members" >&2
 fi
 . ${CONFIGFILE}
 
-if [ -z ${DATA} ] || [ -z ${KERNEL} ] || [ -z ${DELIMITER} ];
+if [ -z ${DATA} ] || [ -z ${KERNEL} ] || [ -z ${DELIMITER} ] || [ -z ${KERNEL_TREE} ];
 then
-	echo "Vars DATA, KERNEL, or DELIMITER are not set!" >&2
+	echo "Vars DATA, KERNEL, DELIMITER, or KERNEL_TREE are not set!" >&2
 	exit 1
 fi
 
@@ -127,9 +127,9 @@ fi
 #GDB='cgdb --args'
 
 if echo $DATA | egrep -q '.bz2$'; then
-	$VALGRIND $GDB ${CONVERT_BINARY} -t ${DATA_TYPES} -k $KERNEL -b ${FN_BLACK_LIST} -m ${MEMBER_BLACK_LIST} -d "${DELIMITER}" <( eval pbzip2 -d < $DATA ${HEAD_CMD} ) > ${CONV_OUTPUT} 2>&1
+	$VALGRIND $GDB ${CONVERT_BINARY} -g ${KERNEL_TREE} -t ${DATA_TYPES} -k $KERNEL -b ${FN_BLACK_LIST} -m ${MEMBER_BLACK_LIST} -d "${DELIMITER}" <( eval pbzip2 -d < $DATA ${HEAD_CMD} ) > ${CONV_OUTPUT} 2>&1
 elif echo $DATA | egrep -q '.gz$'; then
-	$VALGRIND $GDB ${CONVERT_BINARY} -t ${DATA_TYPES} -k $KERNEL -b ${FN_BLACK_LIST} -m ${MEMBER_BLACK_LIST} -d "${DELIMITER}" <( eval gzip -d < $DATA ${HEAD_CMD} ) > ${CONV_OUTPUT} 2>&1
+	$VALGRIND $GDB ${CONVERT_BINARY} -g ${KERNEL_TREE} -t ${DATA_TYPES} -k $KERNEL -b ${FN_BLACK_LIST} -m ${MEMBER_BLACK_LIST} -d "${DELIMITER}" <( eval gzip -d < $DATA ${HEAD_CMD} ) > ${CONV_OUTPUT} 2>&1
 else
 	echo "no idea what to do with filename extension of $DATA" >&2
 	exit 1
