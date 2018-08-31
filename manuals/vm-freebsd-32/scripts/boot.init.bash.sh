5afdffe83d2cfd4be984fd9594298a6d34ff4a7a
Alexander Lochmann <alexander.lochmann@tu-dortmund.de>
2018-08-31 12:10:37 +0200

    FreeBSD: Updated init script
    Avoid automatically starting the benchmark when a non-LOCKDOC kernel is loaded.
    This makes booting the normal kernel less annoying, because the use does not have to enter a charcater.

--- manuals/vm-freebsd-32/scripts/boot.init.bash.sh
+++ manuals/vm-freebsd-32/scripts/boot.init.bash.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 echo "PRE-INIT SCRIPT"
-echo "Please press a character key to start normal"
-sleep 1
-read -d "\n" -n 1 -t 5 answer
-if [ "$answer" == "" ]; then
-	echo "Starting Bench"
-	bash /lockdoc/run-bench.sh
+IDENT=`uname -i`
+if [ ${IDENT} == "LOCKDOC" ];
+then
+	echo "Please press a character key to start normal"
+	sleep 1
+	read -d "\n" -n 1 -t 5 answer
+	if [ "$answer" == "" ]; then
+		echo "Starting Bench"
+		bash /lockdoc/run-bench.sh
+	else
+		echo ""
+		echo "Starting normal!"
+	fi
+	sleep 1
 else
-	echo ""
-	echo "Starting normal!"
+	echo "Found normal kernel. Continuing boot process..."
 fi
-sleep 1
-
