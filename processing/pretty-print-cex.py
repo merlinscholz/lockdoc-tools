fd140d2141cd53bb3bbfbc4a89ba1c4cdaae6cca
Alexander Lochmann <alexander.lochmann@tu-dortmund.de>
2018-10-03 14:26:46 +0200

    pretty-print-cex.py: Fixed whitespacing

--- processing/pretty-print-cex.py
+++ processing/pretty-print-cex.py
@@ -116,28 +116,28 @@ tr.line_heading {
 }
 
 .accordion {
-    background-color: #eee;
-    color: #444;
-    cursor: pointer;
-    padding: 18px;
-    width: 100%;
-    text-align: left;
-    border: none;
-    outline: none;
-    transition: 0.4s;
+	background-color: #eee;
+	color: #444;
+	cursor: pointer;
+	padding: 18px;
+	width: 100%;
+	text-align: left;
+	border: none;
+	outline: none;
+	transition: 0.4s;
 }
 
 /* Add a background color to the button if it is clicked on (add the .active class with JS), and when you move the mouse over it (hover) */
 .active, .accordion:hover {
-    background-color: #ccc;
+	background-color: #ccc;
 }
 
 /* Style the accordion panel. Note: hidden by default */
 .stacktrace {
-    /*padding: 0 18px;*/
-    background-color: white;
-    display: none;
-    overflow: hidden;
+	/*padding: 0 18px;*/
+	background-color: white;
+	display: none;
+	overflow: hidden;
 }
 </style>
 </head>
