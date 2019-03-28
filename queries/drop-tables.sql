dc8601854215ed53c9fdfbb75e026b1291861fa3
Alexander Lochmann <info@alexander-lochmann.de>
2019-03-28 14:09:02 +0100

    PostgreSQL #7 - queries/drop-tables.sql

--- queries/drop-tables.sql
+++ queries/drop-tables.sql
@@ -1,1 +1,3 @@
-drop table if exists accesses,allocations,data_types,lock_symbols,locks,locks_held,structs_layout,txns,member_names,function_blacklist,member_blacklist,stacktraces,subclasses;
+drop table if exists accesses,allocations,data_types,locks,locks_held,structs_layout,txns,member_names,function_blacklist,member_blacklist,stacktraces,subclasses;
+drop type if exists access_type, sub_lock_type, irq_sync_type;
+drop sequence if exists function_blacklist_seq;
