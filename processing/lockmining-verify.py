7618d8c27d53a054b0287618a61555234d2f42a1
Alexander Lochmann <alexander.lochmann@.tu-dortmund.de>
2019-11-28 09:25:08 +0100

    lockminig-verify.py: Calculate fraction of correctly predicted hypothesis

---
+++ processing/lockmining-verify.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python
+# A.Lochmann 2019
+# This scripts verifies the mined results against the existing documentation.
+# ./lockmining-verify.py <groundtruth csv> <winner hypotheses csv>
+
+from __future__ import print_function
+import sys
+import csv
+import re
+import argparse
+from pprint import pprint
+import logging
+import util
+
+logging.basicConfig()
+LOGGER = logging.getLogger(__name__)
+
+if __name__ == '__main__':
+
+	parser = argparse.ArgumentParser()
+	parser.add_argument('-v', '--verbose', action='store_true', help='Be verbose')
+	parser.add_argument('gtruthCSV', help='Input file containing the ground truth')
+	parser.add_argument('hypothesisCSV', help='Input file containing preditions made by the hypothesizer')
+	args = parser.parse_args()
+
+	groundtruthDict = dict()
+	hypothesisDict = util.readHypothesesDict(args.hypothesisCSV)
+	if args.verbose:
+		LOGGER.setLevel(logging.DEBUG)
+	else:
+		LOGGER.setLevel(logging.INFO)
+
+	tempFile = open(args.gtruthCSV,'rb')
+	tempReader = csv.DictReader(tempFile, delimiter=';')
+	for line in tempReader:
+		key = (line['datatype'], line['member'] ,line['accesstype'])
+		if key in groundtruthDict:
+			LOGGER.error('Key %s does already exist in groundtruthDict', key)
+		groundtruthDict[key] = line['rule']
+	tempFile.close()
+	LOGGER.debug('Read %d locking rules from "%s"', len(groundtruthDict), args.gtruthCSV)
+
+	totalRules = 0
+	matchedRules = 0
+	for key, lockingRule in groundtruthDict.iteritems():
+		if key in hypothesisDict:
+			locksHeldDict = hypothesisDict[key]['locks']
+			if len(locksHeldDict) > 1 :
+				LOGGER.error('More than one lock hypothesis for %s', key)
+			totalRules = totalRules + 1
+			if len(locksHeldDict.keys()) > 1 :
+				LOGGER.error("More than one hypotheses for %s" % (key))
+			locksHeld = locksHeldDict.keys()[0]
+			if re.match('^' + lockingRule + '$',locksHeld):
+				matchedRules = matchedRules + 1
+	print("totalrules;matched;percentage")
+	print("%d;%d;%3.2f" % (totalRules, matchedRules, util.calcPercentage(totalRules, matchedRules)))
