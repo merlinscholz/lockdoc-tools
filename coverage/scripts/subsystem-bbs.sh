ca01da09cfb6e56513aba6311938deefff495b08
Horst Schirmeier <horst@schirmeier.com>
2020-08-24 16:47:37 +0200

    coverage: match subsystem with regex

--- coverage/scripts/subsystem-bbs.sh
+++ coverage/scripts/subsystem-bbs.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 if [ $# -ne 3 ]; then
 	echo "usage: $0 path/to/vmlinux all-kernel-bbs.map pattern" >&2
-	echo "pattern is the substring that is searched for, e.g. '/fs/'" >&2
+	echo "pattern is the bash-regex pattern that is searched for, e.g. '/fs/|/mm/|fs\\.h|mm\\.h'" >&2
 	exit 1
 fi
-	
+
 addr2line -a -e "$1" < "$2" | \
 while read ADDR; do
 	read LINE
-	if [[ $LINE == *$3* ]]; then
+	if [[ $LINE =~ $3 ]]; then
 		echo ${ADDR#0x} # strip leading "0x"
 	fi
 done
