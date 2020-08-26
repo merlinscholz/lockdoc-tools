dbc8e20b99a9db3eaa78833ebdb9f0a2ede2ce5c
Horst Schirmeier <horst@schirmeier.com>
2020-08-26 21:07:47 +0200

    coverage: some helper script fail safety

--- coverage/scripts/fixup-syzkaller-maps.sh
+++ coverage/scripts/fixup-syzkaller-maps.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
-INDIR=syzkaller-maps
-OUTDIR=syzkaller-maps-fixpc
 
-for f in $INDIR/*; do
+DELTA=5
+
+if [ $# -lt 1 ]; then
+	echo "usage: $(basename $0) dir-to-transform ..." >&2
+	echo "" >&2
+	echo "Adds $DELTA to all addresses in all files in all directories. " >&2
+	echo "" >&2
+	echo "Achtung: This tool will modify files in place without backup." >&2
+	exit 1
+fi
+
+set -e
+
+TMP=$(mktemp)
+trap "rm -f $TMP" EXIT
+
+find "$@" -type f | \
+while read f; do
 	echo "processing $f ..."
-	OUT=syzkaller-maps-fixpc/$(basename $f)
 	(
 	echo obase=16
 	echo ibase=16
@@ -12,5 +26,6 @@ for f in $INDIR/*; do
 	while read line; do
 		echo $line+5
 	done
-	) | bc | tr A-Z a-z > $OUT
+	) | bc | tr A-Z a-z > $TMP
+	mv $TMP $f
 done
