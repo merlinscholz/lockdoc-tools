06ed7cc2364e0a76aadcfb0d2fffe2d89cfc75b7
Alexander Lochmann <alexander.lochmann@tu-dortmund.de>
2020-09-30 17:06:16 +0200

    Processing: Replaced evaluate-sel-strategy.sh by a better python version

---
+++ processing/aux/eval-selection-strategy.py
@@ -0,0 +1,83 @@
+#!/usr/bin/python3
+from __future__ import print_function
+import os, sys, os.path
+import argparse
+import logging
+import tempfile
+import subprocess
+import numpy
+
+logging.basicConfig()
+LOGGER = logging.getLogger(__name__)
+LOGGER.setLevel(logging.INFO)
+
+strategies = {
+	"sharpen": {
+		'start': 0.0,
+		'step': 0.5,
+		'end': 5.0
+	},
+	"bottomup": {
+		'start': 100.0,
+		'step': -0.5,
+		'end': 95.0
+	},
+	"topdown": {
+		'start': 100.0,
+		'step': -0.5,
+		'end': 95.0
+	}
+}
+
+def main():
+	basedir = os.path.dirname(sys.argv[0])
+	argv = sys.argv[1:]
+
+	parser = argparse.ArgumentParser(prog='PROG')
+	parser.add_argument('--groundtruth-csv', help='CSV containing the documented locking rules', required=True)
+	parser.add_argument('--hypothesizer-input', help='', required=True)
+	parser.add_argument('--selection-strategy', help='Just evaluate a particular selections strategy')
+
+	args = parser.parse_args(argv)
+
+	groundtruthCSV = args.groundtruth_csv
+	hypoInput = args.hypothesizer_input
+	selStrategy = args.selection_strategy
+	outFile = sys.stdout
+	if selStrategy:
+		print("selStrategy = %s" % (selStrategy))
+	print("groundtruthCSV = %s, hypoInput = %s" % (groundtruthCSV, hypoInput))
+
+
+
+	print("strategy;parameter;totalrules;matched;percentage", file = outFile)
+
+	for key in strategies.keys():
+		params = strategies[key]
+		for i in numpy.arange(params['start'], params['end'] + params['step'], params['step']):
+			with tempfile.NamedTemporaryFile() as tempOut:
+				cmd = basedir + '/../../hypothesizer/hypothesizer -g %s -f %.2f -a %.2f -s member -r csvwinner %s' % (key, i, i, hypoInput)
+				LOGGER.debug("Running '%s'" % (cmd))
+				hypothesizer = subprocess.Popen(cmd.split(),
+								stdout = tempOut,
+								stderr = subprocess.PIPE)
+				stdout, stderr = hypothesizer.communicate()
+				if hypothesizer.returncode != 0:
+					LOGGER.error("Error running: '%s'\n%s" % (cmd, stderr.decode()))
+					continue
+				cmd = basedir + '/../locking-rule-minig-verify.py %s %s' % (groundtruthCSV, tempOut.name)
+				LOGGER.debug("Running '%s'" % (cmd))
+				lock_verify = subprocess.Popen(cmd.split(),
+								stdout = subprocess.PIPE,
+								stderr = subprocess.PIPE)
+				stdout, stderr = lock_verify.communicate()
+				if lock_verify.returncode != 0:
+					LOGGER.error("Error running: '%s'\n%s" % (cmd, stderr.decode()))
+				lines = stdout.decode().splitlines()
+				if len(lines) > 2:
+					LOGGER.error("locking-rule-minig-verify.py returned more than 2 lines: %s" % (lines))
+					sys.exit(1)
+				print("%s;%s" % (key, lines[1]), file = outFile)
+
+if __name__ == "__main__":
+	main()
