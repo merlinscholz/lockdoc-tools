24b1551a6542a1750cef99dac6e2a5a33175c17a
Alexander Lochmann <alexander.lochmann@.tu-dortmund.de>
2020-10-06 16:21:38 +0200

    locking-rule-minig-verify.py: Fixed denominator
    The denominator must comprise all tuples (data type, member, access type) that have at least one hypothesis.
    Prior to this commit, it just comprised those tuples that have a winning hypothesis.
    Some strategies do not determine a winner for a particular tuple.

--- processing/locking-rule-minig-verify.py
+++ processing/locking-rule-minig-verify.py
@@ -21,11 +21,13 @@ if __name__ == '__main__':
 	parser = argparse.ArgumentParser()
 	parser.add_argument('-v', '--verbose', action='store_true', help='Be verbose')
 	parser.add_argument('gtruthCSV', help='Input file containing the ground truth')
-	parser.add_argument('hypothesisCSV', help='Input file containing preditions made by the hypothesizer (winning hypotheses only)')
+	parser.add_argument('hypoAllCSV', help='Input file containing preditions made by the hypothesizer (all hypotheses)')
+	parser.add_argument('hypoWinnerCSV', help='Input file containing preditions made by the hypothesizer (winning hypotheses only)')
 	args = parser.parse_args()
 
 	groundtruthDict = dict()
-	hypothesisDict = util.readHypothesesDict(args.hypothesisCSV)
+	hypoAllDict = util.readHypothesesDict(args.hypoAllCSV)
+	hypoWinnerDict = util.readHypothesesDict(args.hypoWinnerCSV)
 	if args.verbose:
 		LOGGER.setLevel(logging.DEBUG)
 	else:
@@ -44,8 +46,11 @@ if __name__ == '__main__':
 	totalRules = 0
 	matchedRules = 0
 	for key, lockingRule in groundtruthDict.items():
-		if key in hypothesisDict:
-			locksHeldDict = hypothesisDict[key]['locks']
+		if key in hypoWinnerDict and key not in hypoAllDict:
+			LOGGER.error("key '%s' is present in winner csv but not in the overall list" % (key))
+			continue
+		if key in hypoWinnerDict:
+			locksHeldDict = hypoWinnerDict[key]['locks']
 			if len(locksHeldDict) > 1 :
 				LOGGER.error('More than one lock hypothesis for %s', key)
 			totalRules = totalRules + 1
@@ -54,5 +59,7 @@ if __name__ == '__main__':
 			locksHeld = list(locksHeldDict.keys())[0]
 			if re.match('^' + lockingRule + '$',locksHeld):
 				matchedRules = matchedRules + 1
+		elif key in hypoAllDict:
+			totalRules = totalRules + 1
 	print("totalrules;matched;percentage")
 	print("%d;%d;%3.2f" % (totalRules, matchedRules, util.calcPercentage(totalRules, matchedRules)))
