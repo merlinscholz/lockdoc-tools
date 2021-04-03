1bb997566b593b214bdd22f00660e44c49ea31fb
Alexander Lochmann <info@alexander-lochmann.de>
2021-04-03 13:25:52 +0200

    locking-rule-minig-verify.py: Fixed counting + respect conflict list
    Output all values: observed total, r, and w.
    Use observed {R,W} counter as basis for percentage {R,W}.
    
    Respect conflict list when looking for the matching hypothesis

--- processing/locking-rule-minig-verify.py
+++ processing/locking-rule-minig-verify.py
@@ -51,22 +51,37 @@ if __name__ == '__main__':
 			LOGGER.error("key '%s' is present in winner csv but not in the overall list" % (key))
 			continue
 		if key[0] not in results:
-                    results[key[0]] = { 'totalRules': 0.0, 'matchedRulesR': 0.0, 'matchedRulesW': 0.0}
+			results[key[0]] = { 'observedRulesR': 0.0, 'observedRulesW': 0.0, 'matchedRulesR': 0.0, 'matchedRulesW': 0.0}
 		if key in hypoWinnerDict:
 			locksHeldDict = hypoWinnerDict[key]['locks']
 			if len(locksHeldDict) > 1 :
 				LOGGER.error('More than one lock hypothesis for %s', key)
-			results[key[0]]['totalRules'] += 1
+			if key[2] == 'w':
+				results[key[0]]['observedRulesR'] += 1
+			elif key[2] == 'r':
+				results[key[0]]['observedRulesW'] += 1
+			else:
+				print("Unknown access type: %s" % (key[2]), file = sys.stderr)
+				sys.exit(1)
 			if len(locksHeldDict.keys()) > 1 :
 				LOGGER.error("More than one hypotheses for %s" % (key))
-			locksHeld = list(locksHeldDict.keys())[0]
-			if re.match('^' + lockingRule + '$',locksHeld):
-				if key[2] == 'w':
-        				results[key[0]]['matchedRulesW'] += 1
-				elif key[2] == 'r':
-        				results[key[0]]['matchedRulesR'] += 1
-				else:
-					print("Unknown access type: %s" % (key[2]))
+				sys.exit(1)
+			locksHeldList = []
+			locksHeldList.append(list(locksHeldDict.keys())[0])
+			for locks, locksStats in locksHeldDict.items():
+				locksHeldList.append(locks)
+				for conflictLocks in locksStats['conflictlist']:
+					locksHeldList.append(conflictLocks)
+			found = False
+			for locksHeld in locksHeldList:
+				if re.match('^' + lockingRule + '$',locksHeld):
+					if key[2] == 'w':
+						results[key[0]]['matchedRulesW'] += 1
+					elif key[2] == 'r':
+						results[key[0]]['matchedRulesR'] += 1
+					found = True
+					break;
+			if found:
 				if args.comparable:
 					print("YES:{0}".format(key), file = sys.stderr)
 			else:
@@ -77,30 +92,45 @@ if __name__ == '__main__':
 		elif key in hypoAllDict:
 			if args.comparable:
 				print("NO :{0}".format(key), file = sys.stderr)
-			results[key[0]]['totalRules'] += 1
+			if key[2] == 'w':
+				results[key[0]]['observedRulesR'] += 1
+			elif key[2] == 'r':
+				results[key[0]]['observedRulesW'] += 1
+			else:
+				print("Unknown access type: %s" % (key[2]), file = sys.stderr)
+				sys.exit(1)
 
-	totalRules = 0.0
-	matchedRulesR = 0.0
-	matchedRulesW = 0.0
-	print("data_type;totalrules;matched;matched_r;matched_w;percentage;percentage_r;percentage_w")
+	allObservedR = 0.0
+	allObservedW = 0.0
+	allMatchedR = 0.0
+	allMatchedW = 0.0
+	print("data_type;observedrules;observedrules_r;observedrules_w;matched;matched_r;matched_w;percentage;percentage_r;percentage_w")
 	for key, values in results.items():
-		totalMatched = values['matchedRulesR'] + values['matchedRulesW']
-		print("%s;%d;%d;%d;%d;%3.2f;%3.2f;%3.2f" %
-				(key, values['totalRules'],
-				 totalMatched,
+		matched = values['matchedRulesR'] + values['matchedRulesW']
+		observed = values['observedRulesR'] + values['observedRulesW']
+		print("%s;%d;%d;%d;%d;%d;%d;%3.2f;%3.2f;%3.2f" %
+				(key,
+				 observed,
+				 values['observedRulesR'],
+				 values['observedRulesW'],
+				 matched,
 				 values['matchedRulesR'],
 				 values['matchedRulesW'],
-				 util.calcPercentage(values['totalRules'], totalMatched),
-				 util.calcPercentage(values['totalRules'], values['matchedRulesR']),
-				 util.calcPercentage(values['totalRules'], values['matchedRulesW'])))
-		totalRules += values['totalRules']
-		matchedRulesR += values['matchedRulesR']
-		matchedRulesW += values['matchedRulesW']
-	print("total;%d;%d;%d;%d;%3.2f;%3.2f;%3.2f" %
-			(totalRules,
-			 matchedRulesR + matchedRulesW,
-			 matchedRulesR,
-			 matchedRulesW,
-			 util.calcPercentage(totalRules, matchedRulesR + matchedRulesW),
-			 util.calcPercentage(totalRules, matchedRulesR),
-			 util.calcPercentage(totalRules, matchedRulesW)))
+				 util.calcPercentage(observed, matched),
+				 util.calcPercentage(values['observedRulesR'], values['matchedRulesR']),
+				 util.calcPercentage(values['observedRulesW'], values['matchedRulesW'])))
+		allObservedR += values['observedRulesR']
+		allObservedW += values['observedRulesW']
+		allMatchedR += values['matchedRulesR']
+		allMatchedW += values['matchedRulesW']
+	allObservedRules = allObservedR + allObservedW
+	print("total;%d;%d;%d;%d;%d;%d;%3.2f;%3.2f;%3.2f" %
+			(allObservedRules,
+			 allObservedR,
+			 allObservedW,
+			 allMatchedR + allMatchedW,
+			 allMatchedR,
+			 allMatchedW,
+			 util.calcPercentage(allObservedRules, allMatchedR + allMatchedW),
+			 util.calcPercentage(allObservedRules, allMatchedR),
+			 util.calcPercentage(allObservedRules, allMatchedW)))
