496bbe43e76752d8e3c8286faad251e9c748efae
Alexander Lochmann <alexander.lochmann@.tu-dortmund.de>
2023-03-20 11:28:12 +0100

    aux - csv2html.sh: Fixed it

--- processing/aux/csv2html.sh
+++ processing/aux/csv2html.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 echo "<table border=1>" ;
-print_header=false
+print_header=true
 while read INPUT ; do
 	if $print_header;then
-	    echo "<tr><th>$INPUT" | sed -e 's/:[^,]*\(,\|$\)/<\/th><th>/g'
+	    echo "<tr><th>$INPUT" | sed -e 's/\(;\|$\)/<\/th><th>/g'
 		print_header=false
+		continue
 	fi
 	echo "<tr><td>${INPUT//;/</td><td>}</td></tr>" ;
 done;
