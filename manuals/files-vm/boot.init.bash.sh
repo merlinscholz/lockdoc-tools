7d34546d806b69a2491c88c0944f2e895cd7da4c
Merlin Scholz <merlin@scholz.ruhr>
2023-07-10 15:25:20 +0200

    Update run-bench and init scripts for NetBSD use

--- manuals/files-vm/boot.init.bash.sh
+++ manuals/files-vm/boot.init.bash.sh
@@ -1,19 +1,20 @@
-#!/usr/local/bin/bash
+#!/bin/bash
 echo "PRE-INIT SCRIPT"
-IDENT=`uname -i`
-if [ ${IDENT} == "LOCKDOC" ];
+IDENT=`uname -v`
+if uname -v | grep "LOCKDOC";
 then
-	echo "Please press a character key to start normal"
-	sleep 1
-	read -d "\n" -n 1 -t 5 answer
-	if [ "$answer" == "" ]; then
-		echo "Starting Bench"
-		/usr/local/bin/bash /lockdoc/run-bench.sh
-	else
-		echo ""
-		echo "Starting normal!"
-	fi
-	sleep 1
+        echo "Please press a character key to start normal"
+        sleep 1
+        read -d "\n" -n 1 -t 5 answer
+        if [ "$answer" == "" ]; then
+                mount -o rw /
+                echo "Starting Bench"
+                /usr/pkg/bin/bash /lockdoc/run-bench.sh
+        else
+                echo ""
+                echo "Starting normal!"
+        fi
+        sleep 1
 else
-	echo "Found normal kernel. Continuing boot process..."
+        echo "Found normal kernel. Continuing boot process..."
 fi
