c5aa27abc9c2533dacd7c886626310637bc6bffa
Alexander Lochmann <alexander.lochmann@tu-dortmund.de>
2023-10-13 17:09:48 +0200

    coverage: Filter kernel src dir from addr2line

--- coverage/scripts/subsystem-bbs.sh
+++ coverage/scripts/subsystem-bbs.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
-if [ $# -ne 3 ]; then
-	echo "usage: $0 path/to/vmlinux all-kernel-bbs.map pattern" >&2
+if [ $# -ne 4 ]; then
+	echo "usage: $0 path/to/kernel/src path/to/vmlinux all-kernel-bbs.map pattern" >&2
 	echo "pattern is the bash-regex pattern that is searched for, e.g. '/fs/|/mm/|fs\\.h|mm\\.h'" >&2
 	exit 1
 fi
 
 LASTADDR=NONE
 
-addr2line -a --inlines -e "$1" < "$2" | \
+addr2line -a --inlines -e "$2" < "$3" | \
 while read LINE; do
 	if [[ $LINE == 0x* ]]; then
 		LASTADDR=${LINE#0x} # strip leading "0x"
-	elif [[ $LINE =~ $3 && $LASTADDR != NONE ]]; then
-		echo $LASTADDR
-		# make sure we output each address only once
-		LASTADDR=NONE
+	else
+		LINE=`echo "$LINE" | sed -e "s%$1%%g"`
+		if [[ $LINE =~ $3 && $LASTADDR != NONE ]]; then
+			echo $LASTADDR
+			# make sure we output each address only once
+			LASTADDR=NONE
+		fi
 	fi
 done
